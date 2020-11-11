f37d992f69111e5c69a28b36a92362b9ae20087e
rpiwangdabai <tjuhaowang@gmail.com>
2020-11-11 17:03:35 -0500

    first commit

---
+++ portfoliolib/securities.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2020-11-09 21:03:01
+# @Author  : Hao Wang (tjuhaowang@gmail.com)
+# @Link    : link
+# @Version : 1.0.0
+"""
+module includes all securites
+"""
+# import os
+# import pandas as pd
+# import numpy as np
+# import Config.userConfig as ucfg
+# import Config.dataConfig as dcfg
+
+class Security():
+    """
+    Base class for alll securities
+    """
+    def __init__(self):
+        """
+        __init__(self):
+        """
+
+    def calculate_security_correlation(self, base_security):
+        """
+        alculateSecurityCorrelation(self, baseSbase_securityecurity)
+        """
+
+
+    def calculate_security_std(self):
+        """
+        calculateSecurityStd(self):
+        """
