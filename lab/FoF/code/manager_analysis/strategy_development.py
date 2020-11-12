75e064271573947e39472e4afd4c280b8f21bb20
YiZhuoMa <mazhuoyi3@gmail.com>
2020-11-12 18:10:14 +0800

    update strategy development

--- lab/FoF/code/manager_analysis/strategy_development.py
+++ lab/FoF/code/manager_analysis/strategy_development.py
@@ -15,7 +15,6 @@ import math
 import collections
 import matplotlib.patches as mpatches
 from mlfinlab.portfolio_optimization.clustering import HierarchicalEqualRiskContribution
-
 from mlfinlab.portfolio_optimization.clustering import HierarchicalRiskParity
 # =============================================================================
 # import data
@@ -37,20 +36,15 @@ while ticks:
     tick = ticks.popleft()
     sql_cmd = "SELECT * FROM `" + tick + '`;'
     fund_data = pd.read_sql(sql=sql_cmd, con=engine)
-    fund_data = fund_data[fund_data['end_date'] < 20200101]
-    fund_init_date = fund_data.iloc[-1]['end_date']
+    fund_data = fund_data[fund_data['end_date'] < '20200101']
+    fund_data = fund_data.drop_duplicates(subset = ['end_date'])
+
     if i == 0:
-        portfolio_adj_nav['end_date'] = fund_data['end_date']
-        portfolio_adj_nav[tick] = fund_data['adj_nav']
-        closest_date = fund_init_date
+        portfolio_adj_nav = fund_data[['end_date','adj_nav']].copy()
+        portfolio_adj_nav = portfolio_adj_nav.rename(columns = {'adj_nav':tick})
     else:
-        if fund_init_date >= closest_date:
-            closest_date = fund_init_date
-            portfolio_adj_nav = portfolio_adj_nav[portfolio_adj_nav['end_date']
-                                                  > closest_date]
-            portfolio_adj_nav[tick] = fund_data['adj_nav']
-        else:
-            portfolio_adj_nav[tick] = fund_data[fund_data['end_date'] > closest_date]['adj_nav']
+        portfolio_adj_nav = pd.merge(portfolio_adj_nav,fund_data[['end_date','adj_nav']],how = 'inner')
+        portfolio_adj_nav = portfolio_adj_nav.rename(columns = {'adj_nav':tick})
     i += 1
     print(i)
 
@@ -110,4 +104,74 @@ plt.title('Pearson Correlation of Features',y=1.05,size=45)
 sns.heatmap(portfolio_adj_nav.astype(float).corr(),linewidths=0.1,vmax=1.0,
             square=True,linecolor='white',annot=True)
 plt.show()
+
+
+
+# =============================================================================
+# K-means
+# =============================================================================
+from sklearn.cluster import KMeans
+from sklearn import metrics  
+# tansfer 
+K = [i for i in range(2,10)]
+portfolio_adj_nav_t = portfolio_adj_nav.T
+ch_index = []
+sc = []
+SSE = []
+for k in K:
+    estimator = KMeans(n_clusters = k).fit(portfolio_adj_nav_t)
+    y_pred  = estimator.predict(portfolio_adj_nav_t)
+    # ch_index.append(metrics.calinski_harabasz_score(portfolio_adj_nav_t, y_pred))
+    sc.append(metrics.silhouette_score(portfolio_adj_nav_t, y_pred,metric='euclidean'))
+    # SSE.append(estimator.inertia_)
+# plt.plot(K,ch_index,'s-',color = 'r', label='ch-index')
+plt.plot(K,sc,'o-',color = 'g', label='sc')
+# plt.plot(K,SSE,'o-',color = 'g', label='sc')
+plt.legend(loc = "best")
+plt.show()
+
+# =============================================================================
+#  reset plot corr heatmap
+# =============================================================================
+k = 4
+estimator = KMeans(n_clusters = k).fit(portfolio_adj_nav_t)
+y_pred  = estimator.predict(portfolio_adj_nav_t)
+# fund list
+fund_list = list(portfolio_adj_nav_t.index)
+fund_cluster = pd.DataFrame()
+fund_cluster['fund_ticks'] = fund_list
+fund_cluster['cluster'] = y_pred 
+fund_cluster = fund_cluster.sort_values(by='cluster')
+sorted_list = fund_cluster['fund_ticks']
+portfolio_adj_nav_cluster = portfolio_adj_nav[sorted_list]
+
+#sublists
+portfolio_adj_nav_sub_list = list(fund_cluster[fund_cluster['cluster'] == 0]['fund_ticks'])
+
+# plot corr heatmap
+portfolio_adj_nav_sub = portfolio_adj_nav[portfolio_adj_nav_sub_list]
+plt.figure(figsize=(40,40))
+plt.title('Pearson Correlation of Features',y=1.05,size=45)
+sns.heatmap(portfolio_adj_nav_sub.astype(float).corr(),linewidths=0.1,vmax=1.0,
+            square=True,linecolor='white',annot=True)
+plt.show()
+
+# =============================================================================
+# cluster cor check
+# =============================================================================
+
+portfolio_adj_nav_t['cluster'] = y_pred
+result = portfolio_adj_nav_t.groupby('cluster').mean()
+
+
+plt.figure(figsize=(40,40))
+plt.title('Pearson Correlation of Features',y=1.05,size=45)
+sns.heatmap(result.T.astype(float).corr(),linewidths=0.1,vmax=1.0,
+            square=True,linecolor='white',annot=True)
+plt.show()
+
+
+
+
+
 
